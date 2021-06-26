86f2bd4ed835f6a40b33101a10ece641ae65e719
4yMAXOg <86518911+4yMAXOg@users.noreply.github.com>
2021-06-26 14:21:43 +0300

    Add files via upload

---
+++ storagemap_testing/a4.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+        <?php require("maphead.php"); ?>
+		<?php require("mainhead.php"); ?>
+	<style>
+		#canvas_id {
+			border: solid;
+			/*width: 3508px;
+            height: 2480px;*/
+            width: 29.7cm;
+            height: 21cm;
+		}
+	</style>
+
+		<script src="storagea4.js?random=<?php echo uniqid(); ?>"></script>
+	</head>
+	
+	<body>
+		<?php require("mainbody.php"); ?>
+	</body>
+</html>
