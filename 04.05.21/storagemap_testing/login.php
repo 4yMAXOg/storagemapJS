05e7fcaaf876f66eba47139ebb46e05b464826ef
4yMAXOg <86518911+4yMAXOg@users.noreply.github.com>
2021-06-26 14:31:06 +0300

    Add files via upload

---
+++ 04.05.21/storagemap_testing/login.php
@@ -0,0 +1,25 @@
+<?php
+    ob_start();
+    session_start();
+?>
+
+<h2>Login</h2>
+    <form method="post">
+    <input type="text" name="user">
+    <input type="password" name="password">
+    <input type="submit">
+</form>
+
+<?php
+$users = ["sni" => "e69554604f977e8826cce1240bb9c5a1bbc1fe667edb4826abefb242bce6a36d"];
+$salt = ["sni" => "Theih6eeuo5feiThxieQuoo5xuu8Eepilee0du1P"];
+
+$user = $_POST['user'];
+$password = hash("sha256", $_POST['password'] . $salt[$user]);
+
+if(($password == $users[$user]) && isset($users[$user])) {
+    $_SESSION['user'] = $user;
+    header("Location: editor.php");
+    exit();
+}
+?>
